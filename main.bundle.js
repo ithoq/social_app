7e93e432f241d451fbdd379b4f494a7a05f348af
Office Account <officeaccount@nomans-MacBook-Pro.local>
2017-01-14 12:06:02 +0500

    comited

--- main.bundle.js
+++ main.bundle.js
@@ -578,7 +578,7 @@ var CreateProfileComponent = (function () {
             if (_this.auth.getUser().timelines == null) {
                 _this.timelineService.create({ Name: 'My Private Timeline' }).subscribe(function (data) {
                     var user = _this.auth.getUser();
-                    user.timelines = [{ Id: data.json().payload.TimelineId, Name: 'My Private Timeline 5' }];
+                    user.timelines = [{ Id: data.json().payload.TimelineId, Name: 'My Private Timeline' }];
                     _this.auth.setUser(JSON.stringify(user));
                     var entry = {
                         TimelineId: data.json().payload.TimelineId,
@@ -588,11 +588,13 @@ var CreateProfileComponent = (function () {
                     };
                     _this.entrySerice.addEntry(entry).subscribe(function (data) {
                         _this.router.navigate(['/home']);
+                        location.reload(); // for the time being
                     });
                 }, function (error) { });
             }
             else {
                 _this.router.navigate(['/home']);
+                location.reload(); // for the time being
             }
         }, function (error) {
             console.log(error.json());
